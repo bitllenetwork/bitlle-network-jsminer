8183bb0a2070ecb38d57de9aa91e8cb2517796a0
Ale Xau <alexey.salyk@gmail.com>
2019-06-28 17:57:28 +0300

    first commit

---
+++ BGSMiner.js
@@ -0,0 +1,81 @@
+const config = require('./config');
+const BitlleJs = require('bitllejs');
+var bitllejs = new BitlleJs();
+bitllejs.web3.setProvider(new bitllejs.web3.providers.HttpProvider(config.RPC, 10000));
+bitllejs.txSender.setConsoleLog(true);
+
+
+var GS1 = bitllejs.iGasStation.GS1;
+var decimals = 18;
+var cashOutVal = config.cashOutVal * 10 ** decimals;
+var lastEpoch = 0;
+var tankToMine;
+var txSent;
+
+if (bitllejs.config.GasStation.GS1.version != '0.3.0') throw new Error('iGasStation ERROR: unknown GS1 contract version');
+console.log('GasStation1 address:',GS1.address);
+console.log('Miner address:',config.address);
+
+
+function GetGasTank() {
+    setTimeout(() => {
+        var myTanks = GS1.tanksOfOwner(config.address);
+        console.log('Miner tanks', myTanks.toString());
+        if (!myTanks.length) {
+            if (txSent) {
+                console.log('Mine() wating register gasTank tx...');
+                GetGasTank();
+            } else {
+                GS1.registerGasTank(config.privateKey);
+                txSent = true;
+                GetGasTank();
+            }
+        } else {
+            tankToMine = myTanks[0].toNumber();
+            console.log('will mine to tankID: ' + tankToMine);
+            Mine();
+        }
+
+    }, 15000);
+}
+
+function Mine() {
+    setTimeout(() => {
+        GS1.currentEpoch((e, r) => {
+            if (e) {
+                console.log('Mine() get epoch ERROR:', e);
+                Mine();
+            } else {               
+                if (lastEpoch < r) {
+                    console.log('new epoch:', r.toNumber());
+                    lastEpoch = r;
+                    GetBounty(tankToMine, (e, r) => {
+                        GS1.mine(tankToMine, config.valToMine, config.privateKey);
+                    });
+
+                }
+                //console.log('cur epoch', r.toNumber());
+                Mine();
+            }
+        })
+    }, 15000);
+}
+
+function GetBounty(_tankID, callBack) {
+    GS1.calcBounty(_tankID, (e, r) => {
+        if (e) {
+            console.log('GetBounty error');
+            callBack(e);
+        } else {
+            console.log('current bounty amount', r.bounty.toNumber() / (10 ** decimals), 'BTL');
+            if (r.bounty >= cashOutVal) {
+                console.log('Bounty receiving of', r.bounty.toNumber() / (10 ** decimals), 'BTL');
+                GS1.getBounty(_tankID, config.privateKey, callBack);
+            } else {
+                callBack();
+            }
+        }
+    });
+}
+
+GetGasTank();                                  
---
+++ README.md
@@ -0,0 +1,52 @@
+# BitlleGasStation Miner
+
+This script is a simple example built for the implementation of space reservation in [BtitlleGasStation1 smart contract](https://ropsten.etherscan.io/address/0x06836be4e6273d77ee7429e0e6414398a81e3dd2#contracts) and [BTL](https://ropsten.etherscan.io/token/0x752777721dd5fe2db110e9e03b3b8feacff1665e) mining.
+
+## Installation
+
+Use the package manager [npm](https://www.npmjs.com/package/bgsminer) to install BitlleGasStation Miner.
+
+```bash
+npm install bgsminer
+```
+
+## Configuration
+
+Replace the data in the [config.js](./config.js) with your own.  
+
+```JavaScript
+{
+    address: '0x.......................................',
+    privateKey:'0x.......................................................' ,
+    valueToMine: 1,
+    cashOutVal: 500,
+    RPC : 'https://ropsten.infura.io/'
+}
+```
+
+## Usage
+
+```bash
+node BGSMiner
+```
+
+## Descriptions
+
+The script logic is very simple and we can divide it into 4 parts. 
+
+1. Configuration. In order to start one should enter the following to “config.js” file:
+An address to be used for mining
+A private key for this address
+RPC (optionally)
+A number of used slots for the epoch (optionally)
+An amount of accrued BTL for withdrawal, hence, the withdrawal will be processed when the entered amount of BTL is achieved  (optionally)
+
+2. Registration of a container. At the start the script is checking if the entered address has any containers.  In case containers are available, the script always uses the first container from the list (a smart contract returns the list from the last to the first, which means that a container with the bigger ID will be at the beginning of the list). If there are no containers, the script sends automatically a transaction for its creation and waits for a container to be created. As soon as a container is created a space reservation starts.
+
+3. Mining. Once per each 15 seconds the script is checking the number of the current epoch. In case it is bigger than the previous one, the script sends a transaction for mining, with the features mentioned in a Config file. Hence, we get 1 transaction per an epoch. 
+
+4. BTL Withdrawal. As soon as the quantity of accrued for withdrawal BTL tokens exceeds the set up number in Settings, the withdrawal to the entered address is processed automatically.
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
